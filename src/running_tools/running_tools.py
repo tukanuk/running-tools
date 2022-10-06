6f364fe75e24a5210978157cb0649d7d025451df
Ben Davidson <benjamin@idavidson.ca>
2022-10-05 22:28:46 -0400

    Most basic pace calculator Working on a full typer/rich UI

--- src/running_tools/running_tools.py
+++ src/running_tools/running_tools.py
@@ -3,11 +3,14 @@
 from typing import Optional
 from pathlib import Path
 import time
+from xml.dom import minicompat
 
 import typer
+from rich import print
 from rich.console import Console
 from rich.table import Table
 from rich.progress import track
+from rich.panel import Panel
 
 
 running = typer.Typer()
@@ -15,22 +18,37 @@ console = Console()
 
 APP_NAME = "MY cli app"
 
+
 @running.command()
 def pace(
-    first: Optional[str] = typer.Argument(None, help="The first name of the person"),
-    last: str = "",
-    middle: str = "",
+    distance: str = typer.Argument(..., help="The distance in meters"),
+    time: str = typer.Argument(..., help="The time in mm:ss"),
 ):
     """
-    Callin you by your name, like you deserve
+    The most basic pace calculator
 
-    options include --last name and --middle name
+    options include --distance in meters and --time in mm:ss
     """
-    print(f"Hello {first} {middle} {last}")
+    minutes = int((time.split(":")[0]))
+    seconds = int((time.split(":")[1]))
+    time_in_seconds = int(minutes * 60 + seconds)
+
+    print(f"time in seconds {time_in_seconds}")
+
+    seconds_per_km = int(time_in_seconds / (int(distance) / 1000))
+
+    minutes = int(seconds_per_km / 60)
+    seconds = seconds_per_km - minutes * 60
+
+    print(
+        Panel.fit(
+            f"Running [yellow]{distance}[/yellow] meters in [purple]{time}[/purple] gives you a pace of [green bold]{minutes}:{seconds:02}[/green bold] /km"
+        )
+    )
 
 
 @running.command()
-def goodbye(name: str, formal: bool = False):
+def goodbye(name: str, formal: bool = False):git 
     if formal:
         print(f"Have a good night {name}, sir!")
     else:
@@ -51,10 +69,10 @@ def main(name: str):
     print(f"Hello {name}")
 
     total = 0
-    for value in track(range(100), description= "Processing"):
+    for value in track(range(100), description="Processing"):
         time.sleep(0.1)
         total = total + 1
-    
+
         app_dir = typer.get_app_dir(APP_NAME)
     app_dir_path = Path(app_dir)
     app_dir_path.mkdir(parents=True, exist_ok=True)
