54021ccf1acee0476d40068741eeb95e42bfdd19
Ben Davidson <benjamin@idavidson.ca>
2022-10-05 21:43:28 -0400

    playground setup

--- running-tools/__main__.py
+++
@@ -1,9 +0,0 @@
-""" A collection of running tools """
-
-def main() -> None:
-    """ main function """
-    print("Welcome to running tools")
-
-
-if __name__ == "__main__":
-    main()
--- tests/test_runningtools.py
+++ tests/test_runningtools.py
@@ -1,5 +1,19 @@
-from runningtools import __version__
+""" running_tools tests """
+
+from src.running_tools import __version__, __main__
+from src.running_tools import running_tools
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    """Test the version"""
+    assert __version__ == "0.0.1"
+
+
+def test_main():
+    """Test the main"""
+    assert __main__.main() == "Welcome to running tools"
+
+
+def test_get_pace():
+    """Test get_pace"""
+    assert running_tools.get_pace(5, 20) == 4
